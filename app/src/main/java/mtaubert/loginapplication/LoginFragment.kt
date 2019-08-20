958a5f2397dcf54448a8af03c86e49de8d7384ec
Max <m.taubert2015@gmail.com>
2019-08-20 14:26:25 +0100

    Added navigation and bindings to the loginfragment

--- app/src/main/java/mtaubert/loginapplication/LoginFragment.kt
+++ app/src/main/java/mtaubert/loginapplication/LoginFragment.kt
@@ -6,13 +6,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
+import androidx.databinding.DataBindingUtil
+import mtaubert.loginapplication.databinding.FragmentLoginBinding
+
+
+
 
 class LoginFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+
+        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
+            R.layout.fragment_login,container,false)
+        //The complete onClickListener with Navigation
+        binding.signUpButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+        }
+        return binding.root
+
+
     }
 }
