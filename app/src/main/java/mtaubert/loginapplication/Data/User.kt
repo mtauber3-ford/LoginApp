de0868b4f1e3a47dbdb4185e52b0e341e07eb579
Max <m.taubert2015@gmail.com>
2019-09-05 11:28:33 +0100

    Room conversion complete

--- app/src/main/java/mtaubert/loginapplication/Data/User.kt
+++ app/src/main/java/mtaubert/loginapplication/Data/User.kt
@@ -4,5 +4,8 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "User")
-data class User(@PrimaryKey @ColumnInfo(name = "Email") val email: String, @ColumnInfo(name = "Password") val password: String, @ColumnInfo(name = "Name") val name: String)
+@Entity(tableName = "Users_Table")
+data class User(@PrimaryKey @ColumnInfo(name = "email") var email: String,
+                @ColumnInfo(name = "password") var password: String,
+                @ColumnInfo(name = "name") var name: String
+                )
